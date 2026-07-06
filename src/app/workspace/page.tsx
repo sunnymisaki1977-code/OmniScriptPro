6043f3790d989a17ef9225474c6ed41c012bf126
Sunnymisaki <sunnymisaki0624@gmail.com>
2026-07-07 05:44:38 +0800

    fix: resolve import syntax error in workspace page and redirect root to workspace

--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import OmniScript PRO_os from "../../../CanvaApp.js/OmniScript PRO_os";
+import OmniScriptPRO_os from "../../../CanvaApp.js/OmniScript PRO_os";
 
 export default function WorkspacePage() {
-  return <OmniScript PRO_os />;
+  return <OmniScriptPRO_os />;
 }
