0923ae2401ff6cb2d8fbeea4bf1ebb64573062cb
Sunnymisaki <sunnymisaki0624@gmail.com>
2026-07-10 00:49:20 +0800

    chore: remove temporary debug files

--- replace_fetch.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('CanvaApp.js/OmniScript PRO_os.tsx', 'utf8');
-content = content.replace(/fetch\('https:\/\/omni-script-pro\.vercel\.app\/api\//g, "fetch('/api/");
-content = content.replace(/fetch\('\/api\//g, "fetch('https://omni-script-pro.vercel.app/api/");
-content = content.replace(/fetch\(\`\/api\//g, "fetch(`https://omni-script-pro.vercel.app/api/");
-fs.writeFileSync('CanvaApp.js/OmniScript PRO_os.tsx', content);
-console.log('Replaced successfully');
