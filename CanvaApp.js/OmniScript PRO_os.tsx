d35f53839c8b808f81e2fd2c8ad4e0114262cb3c
sunnymisaki <sunnymisaki1977@gmail.com>
2026-07-17 02:36:47 +0800

    Update OmniScript PRO_os.tsx

--- CanvaApp.js/OmniScript PRO_os.tsx
+++ CanvaApp.js/OmniScript PRO_os.tsx
@@ -160,8 +160,8 @@ async function callVercelApi(stepId, context, audienceTheme, userApiKey = "") {
 // ============================================================================
 // 新增：MP4 輪播影片清單 (您可以在此陣列加入多個影片網址)
 const LOADING_VIDEOS_LIST = [
-  "[https://res.cloudinary.com/dhvzfeo7p/video/upload/q_auto/f_auto/v1780920395/_%E5%9C%96%E7%94%9F%E5%8B%95%E7%95%AB%E8%A6%8F%E5%8A%83_Animation_Planning__o5hw6k.mp4](https://res.cloudinary.com/dhvzfeo7p/video/upload/q_auto/f_auto/v1780920395/_%E5%9C%96%E7%94%9F%E5%8B%95%E7%95%AB%E8%A6%8F%E5%8A%83_Animation_Planning__o5hw6k.mp4)",
-  "[https://res.cloudinary.com/dhvzfeo7p/video/upload/v1780920477/_%E5%9C%96%E7%94%9F%E5%8B%95%E7%95%AB%E8%A6%8F%E5%8A%83_Animation_Planning__1_umfge3.mp4](https://res.cloudinary.com/dhvzfeo7p/video/upload/v1780920477/_%E5%9C%96%E7%94%9F%E5%8B%95%E7%95%AB%E8%A6%8F%E5%8A%83_Animation_Planning__1_umfge3.mp4)" // 請替換成您的第二個影片網址
+  "https://res.cloudinary.com/dhvzfeo7p/video/upload/q_auto/f_auto/v1780920395/_%E5%9C%96%E7%94%9F%E5%8B%95%E7%95%AB%E8%A6%8F%E5%8A%83_Animation_Planning__o5hw6k.mp4",
+  "https://res.cloudinary.com/dhvzfeo7p/video/upload/v1780920477/_%E5%9C%96%E7%94%9F%E5%8B%95%E7%95%AB%E8%A6%8F%E5%8A%83_Animation_Planning__1_umfge3.mp4" // 請替換成您的第二個影片網址
 ];
 
 const getInitialStepContent = (stepId, themeText, previousContents = {}) => {
