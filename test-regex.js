07903abba5e82c277866f5177e5ce4e541223b60
sunnymisaki <sunnymisaki1977@gmail.com>
2026-09-11 07:47:46 +0800

    fix: regex for image URL parsing was including trailing brackets

---
+++ test-regex.js
@@ -0,0 +1,3 @@
+const str = '[URL: https://www.winnews.com.tw/wp-content/uploads/2026/09/0909-6-1024x576.webp]';
+const regex = /https?:\/\/[^\s"'<>\)\]]+?\.(?:png|jpg|jpeg|webp|gif)(?:\?[^\s"'<>\)\]]*)?/gi;
+console.log(str.match(regex));
